f6d178b483d989f54fcf5123268cbe25c6aaba96
eloirotava <46318465+eloirotava@users.noreply.github.com>
2026-06-26 20:01:16 -0300

    Run agent on localhost port 8081

--- agente.py
+++ agente.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # Este comando substitui o antigo 'uvicorn app.main:app --reload'
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
-        port=8000,
+        host="127.0.0.1",
+        port=8081,
         reload=False
     )
